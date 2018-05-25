27831045c3a7f693e49f973f7be710c765d0a243
chiayi0327 <30381945+chiayi0327@users.noreply.github.com>
2018-05-25 23:42:36 +0800

    homework
    homework

---
+++ assets/firebase/app.js
@@ -0,0 +1,12 @@
+(function(){
+
+    var config = {
+        apiKey: "AIzaSyAfrZ-E2E1DB8U9UBsBZvrOEXnT7GARBvU",
+        authDomain: "tronclass-1229.firebaseapp.com",
+        databaseURL: "https://tronclass-1229.firebaseio.com",
+        projectId: "tronclass-1229",
+        storageBucket: "tronclass-1229.appspot.com",
+        messagingSenderId: "487813368119"
+    };
+    firebase.initializeApp(config);
+}());
